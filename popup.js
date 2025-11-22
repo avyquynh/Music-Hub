5980c2e7c0c866f45b93c0b81b55e218ad121b7a
avyquynh <avyqchung05@gmail.com>
2025-11-22 16:45:50 -0700

    Embed Pop-Up Test

--- popup.js
+++ popup.js
@@ -1,13 +1,25 @@
 const popup = document.getElementById("genre-popup");
 const genreTitle = document.getElementById("genre-title");
 const genreBody = document.getElementById("genre-body");
+const genreSubgenre = document.getElementById("genre-subgenre");
+const genreExamples = document.getElementById("genre-examples");
+const genreEmbedLink = document.getElementById("genre-embed-link"); 
 const closeButton = document.getElementsByClassName("close-button")[0];
 
 const genreLinks = document.querySelectorAll('.open-popup');
-
-function openPopup(genre, description) {
+function createIframeHTML(embedID) {
+    const iframeSrc = `https://www.youtube.com/embed/${embedID}`;
+    return `<iframe width="325" height="250" src="${iframeSrc}" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowfullscreen></iframe>`;
+}
+function openPopup(genre, description, subgenre, examples, embedID) {
     genreTitle.textContent = genre;
     genreBody.textContent = description;
+    genreSubgenre.innerHTML = subgenre;
+    genreExamples.innerHTML = examples;
+    genreEmbedLink.innerHTML = createIframeHTML(embedID);
     
     popup.style.display = "block";
     document.body.style.overflow = 'hidden'; 
@@ -18,8 +30,11 @@ genreLinks.forEach(link => {
         
         const genreName = this.getAttribute('data-genre');
         const genreDesc = this.getAttribute('data-description');
-        
-        openPopup(genreName, genreDesc);
+        const genreSub = this.getAttribute('data-subgenre');
+        const genreExample = this.getAttribute('data-example');
+        const genreEmbedID = this.getAttribute('data-embed');
+
+        openPopup(genreName, genreDesc, genreSub, genreExample, genreEmbedID);
     });
 });
 
@@ -27,11 +42,13 @@ genreLinks.forEach(link => {
 closeButton.onclick = function() {
     popup.style.display = "none";
     document.body.style.overflow = 'auto'; 
+    genreEmbedLink.innerHTML = '';
 }
 
 window.onclick = function(event) {
     if (event.target == popup) {
         popup.style.display = "none";
         document.body.style.overflow = 'auto'; 
+        genreEmbedLink.innerHTML = '';
     }
 }
