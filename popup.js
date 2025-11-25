2f7c2f41628a873d2b219cbbf4c0f3b0a151b232
avyquynh <avyqchung05@gmail.com>
2025-11-24 21:35:25 -0700

    Genres complete

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ const genreTitle = document.getElementById("genre-title");
 const genreBody = document.getElementById("genre-body");
 const genreSubgenre = document.getElementById("genre-subgenre");
 const genreExamples = document.getElementById("genre-examples");
-const genreEmbedLink = document.getElementById("genre-embed-link"); 
+const genreEmbedLink = document.getElementById("genre-embed-link");
 const closeButton = document.getElementsByClassName("close-button")[0];
 
 const genreLinks = document.querySelectorAll('.open-popup');
@@ -14,16 +14,25 @@ function createIframeHTML(embedID) {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
             allowfullscreen></iframe>`;
 }
-function openPopup(genre, description, subgenre, examples, embedID) {
+function openPopup(genre, description, subgenre, examples, embedCodes) {
     genreTitle.textContent = genre;
     genreBody.textContent = description;
     genreSubgenre.innerHTML = subgenre;
     genreExamples.innerHTML = examples;
-    genreEmbedLink.innerHTML = createIframeHTML(embedID);
+    
+    let allIframesHTML = '';
+    embedCodes.forEach(embedID => {
+        if (embedID) {
+            allIframesHTML += createIframeHTML(embedID);
+        }
+    });
+    
+    genreEmbedLink.innerHTML = allIframesHTML;
     
     popup.style.display = "block";
     document.body.style.overflow = 'hidden'; 
 }
+
 genreLinks.forEach(link => {
     link.addEventListener('click', function(event) {
         event.preventDefault(); 
@@ -32,12 +41,19 @@ genreLinks.forEach(link => {
         const genreDesc = this.getAttribute('data-description');
         const genreSub = this.getAttribute('data-subgenre');
         const genreExample = this.getAttribute('data-example');
-        const genreEmbedID = this.getAttribute('data-embed');
+        
+        const embedIDs = [];
+        let i = 1;
+        let embedID = this.getAttribute(`data-embed${i}`);
+        while (embedID) {
+            embedIDs.push(embedID);
+            i++;
+            embedID = this.getAttribute(`data-embed${i}`);
+        }
 
-        openPopup(genreName, genreDesc, genreSub, genreExample, genreEmbedID);
+        openPopup(genreName, genreDesc, genreSub, genreExample, embedIDs);
     });
 });
-
 
 closeButton.onclick = function() {
     popup.style.display = "none";
